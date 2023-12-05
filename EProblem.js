0df011816b4d00f286f3e8ab33ed55187ec9e56a
Your Name <your_email@example.com>
2023-12-05 00:52:13 -0500

    add function

--- EProblem.js
+++ EProblem.js
@@ -1,1 +1,10 @@
 console.log('Hello, World!'); 
+//QUESTION 2
+function addNumbers(num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Example usage:
+  let result = addNumbers(80, 123);
+  console.log("The sum is: " + result); // Output will be 15
+  
