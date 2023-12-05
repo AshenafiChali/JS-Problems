b76d0cda46e81a8dc2988a75a19aa3a9be5f8ac5
Your Name <your_email@example.com>
2023-12-05 01:04:30 -0500

    even or odd function

--- EProblem.js
+++ EProblem.js
@@ -7,4 +7,16 @@ function addNumbers(num1, num2) {
   // Example usage:
   let result = addNumbers(80, 123);
   console.log("The sum is: " + result); // Output will be 15
-  
+  //question 3
+
+  function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+// Example usage:
+console.log(checkEvenOrOdd(7)); // Output will be "Odd"
+console.log(checkEvenOrOdd(12)); // Output will be "Even"
