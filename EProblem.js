67fe29ae3d397014f2e3ed88f97900488bf7ab01
Your Name <your_email@example.com>
2023-12-10 09:59:00 -0500

    uppercase

--- EProblem.js
+++ EProblem.js
@@ -130,4 +130,4 @@ function convertToUpperCase(str) {
 // Example usage:
 let lowercaseString = "akkam, ashenafi";
 let uppercaseString = convertToUpperCase(lowercaseString);
-console.log(uppercaseString); // Output will be: "HELLO, WORLD!"
+console.log(uppercaseString); // Output will be: "Akkam ASHENAFI!"
